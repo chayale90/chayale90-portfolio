4a12459eb62aa58e93c8616e1a8275883e5f0e9e
Chaya Segal <chayale90@gmail.com>
2023-06-01 23:56:01 +0300

    close menu if press on a link

--- js/burger.js
+++ js/burger.js
@@ -5,41 +5,38 @@ function burgerInit() {
   let nav_open = document.querySelector("#nav_open");
 
   burger_btn.addEventListener("click", function () {
-    // alert("burger work");
     if (!isNavOpen) {
-      burger_btn.innerHTML = `   
-      <i class="fa fa-times x-icon" aria-hidden="true"></i>`
-      // קודם דואג שיוצג
-      nav_open.style.display = "block";
-      setTimeout(function () {
-        // מכיוון שאחנו רוצים להוסיף לו קלאס
-        // ושזה יתבצע באנימציה נצטרך כמה מאיות שניה
-        // אחרי התצוגה להוסיף את הקלאס
-        nav_open.classList.add("nav_open");
-      }, 100);
-      isNavOpen = true;
-
+      openNav();
+    } else {
+      closeNav();
     }
-    else {
-      // מוריד קלאס מסי אס אס 
-      nav_open.classList.remove("nav_open");
-      burger_btn.innerHTML = `   
-      <i class="fa fa-bars " aria-hidden="true"></i>
-      `
-      setTimeout(function () {
-        // ורק לאחר זמן האנימציה אחרי שהיא מסתיימת אנחנו 
-        // נעלים את התגית עצמה לחלוטין כדי 
-        // שלא נוכל ללחוץ עליה
-        nav_open.style.display = "none";
+  });
 
-      }, 600)
+  // Function to open the burger menu
+  function openNav() {
+    burger_btn.innerHTML = `<i class="fa fa-times x-icon" aria-hidden="true"></i>`;
+    nav_open.style.display = "block";
+    setTimeout(function () {
+      nav_open.classList.add("nav_open");
+    }, 100);
+    isNavOpen = true;
+  }
 
-      isNavOpen = false;
+  // Function to close the burger menu
+  function closeNav() {
+    nav_open.classList.remove("nav_open");
+    burger_btn.innerHTML = `<i class="fa fa-bars " aria-hidden="true"></i>`;
+    setTimeout(function () {
+      nav_open.style.display = "none";
+    }, 600);
+    isNavOpen = false;
+  }
 
-    }
-  })
+  // Add click event listener to each navigation link
+  var navLinks = nav_open.getElementsByTagName("a");
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", closeNav);
+  }
 }
-
-
 
 burgerInit();
